21b32e890d98bbacc378856479c66ecbba88fab5
Nicholas Hui <nicholashui8@gmail.com>
2024-09-19 18:28:11 -0700

    Fix vercel route

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,9 @@
 export const Home = () => {
     return (
-        <div>
-            Hi, I'm Nick! I'm a software engineer 👋 
+        <div className="h-screen w-screen bg-zinc-900">
+            <div className="flex items-center justify-center pt-60">
+                <h1 className="text-white font-extrabold text-4xl"> Hi, I'm Nick! I'm a software engineer 👋</h1>
+            </div>
         </div>
     );
 }
