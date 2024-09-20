d7569863d562424ad4d1ef4311e0a9c55994f583
Nicholas Hui <nicholashui8@gmail.com>
2024-09-19 18:11:59 -0700

    Remove default Vite stuff

---
+++ src/Bara.tsx
@@ -0,0 +1,9 @@
+export const Bara = () => {
+    return (
+        <div className="h-screen w-screen bg-lime-500">
+            <div className="h-screen flex items-center justify-center text-9xl">
+                <p>bara</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/Home.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+    return (
+        <div>
+            Hi, I'm Nick! I'm a software engineer 👋 
+        </div>
+    );
+}
